ba6f129576dc1dfd31a15db229466354612e12f5
elliwings <elizabethsokur4@gmail.com>
2024-08-09 20:49:14 +0300

    Added plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
       fontFamily: {
         main: ['Poppins'],
       },
+      width: {
+        widthSize: '1160px',
+      },
+      height: {
+        logoHeight: '100px',
+      },
+      opacity: {
+        svgOpacity: '0.8',
+      },
+      boxShadow: {
+        mainShadow:
+          '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)',
+      },
       colors: {
         primary: '#1d1e21',
         description: '#929395',
@@ -15,4 +28,56 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    function ({ addComponents, theme }) {
+      const headings = {
+        '.heading-primary': {
+          fontSize: '64px',
+          fontWeight: '500',
+          lineHeight: '1.25',
+        },
+        '.heading-secondary': {
+          fontSize: '30px',
+          color: theme('colors.primary'),
+          fontWeight: '600',
+        },
+        '.subtitle': {
+          fontSize: '16px',
+          color: theme('colors.description'),
+          fontWeight: '500',
+          textTransform: 'uppercase',
+        },
+      };
+      addComponents(headings);
+    },
+    function ({ addComponents, theme }) {
+      const buttons = {
+        '.btn-info': {
+          display: 'flex',
+          alignItems: 'center',
+          padding: '16px 8px 16px 0',
+          gap: '20px',
+          fontSize: '20px',
+          color: theme('colors.accent'),
+          textTransform: 'uppercase',
+        },
+        '.btn-arrow': {
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '56px',
+          height: '56px',
+          padding: '16px',
+          borderRadius: '50%',
+          fill: 'currentColor',
+          transition: 'all 0.5s',
+          '&:hover': {
+            backgroundColor: theme('colors.accent'),
+            color: '#ffffff',
+          },
+        },
+      };
+      addComponents(buttons);
+    },
+  ],
 };
